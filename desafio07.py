17ebd143f7701632927022c56d837b8e298b46d2
lucaslacombev <lucaslacombev@gmail.com>
2025-11-19 20:48:03 -0300

    p

---
+++ desafio07.py
@@ -0,0 +1,3 @@
+n1=float(input('Digite a nota da primeira prova: '))
+n2=float(input('Digite a nota da segunda prova: '))
+print(f' A média do aluno é {(n1+n2)/2}')
---
+++ desafio08.py
@@ -0,0 +1,2 @@
+n1=float(input('Digite um valor em metros: '))
+print(f' O valor {n1} em metros é ')
