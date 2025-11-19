2ea319d48f0a51879c4ad7c6da7a9a2c23debc67
lucaslacombev <lucaslacombev@gmail.com>
2025-11-19 20:26:41 -0300

    p

--- aula07a.py
+++ aula07a.py
@@ -1,2 +1,9 @@
-nome=input('Qual é o seu nome?')
-print(f'Prazer em te conhecer, {nome}!')
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+s=n1+n2
+m=n1*n2
+d=n1/n2
+di=n1//n2
+e=n1**n2
+print(f'A soma é {s}, o produto é {m} e a divisão é {d:.3f}', end=' ')
+print(f'A divisão inteira é {di} e a potência é {e}')
